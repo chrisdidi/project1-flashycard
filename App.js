fd5a8e011d5c7e03ac98f9028190d1671b1899ea
chrisdidi <lingmanhau@hotmail.com>
2019-11-08 20:38:59 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>LALALA
-        hello!
-      </Text>
+      <Text>LALALA</Text>
     </View>
   );
 }
